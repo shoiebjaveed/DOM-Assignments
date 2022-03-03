ee418402c1fe8d3825cafd7de88e89ba78a63993
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-03-03 23:35:01 +0530

    Changes as per mentioned in assignments

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //console.log("Hello");
+//adding boder to Header
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+//changing Add Item to Bold and color as green
+ var title = document.getElementById('addItem');
+ title.style.fontWeight = 'bold';
+ title.style.color = 'green';
